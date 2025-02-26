ada49e172d1835e63edcac696551425e0d99553a
Roseanedasilva <167696930+Roseanedasilva1212@users.noreply.github.com>
2025-02-26 01:45:09 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,16 @@
+package PRIMEIRO_PROGRAMA ;
+
+public class OlaMundo {
+
+    Run|Debug
+    public static void main(String[] args ) {
+
+        // 1- Primeiro programa
+
+        Systm.out.println(x:"Meu primeiro programa em java");
+
+        System.out.println("A versão do Java é : " + System.getProperty(key:"java.version"));
+
+        // Alguma coisa
+    }
+}
